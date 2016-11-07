3507b594321187352b426c771b489309a90b1a7f
Mikko Nieminen <mikko.niemimem@gmail.com>
2016-11-07 22:15:22 +0200

    Initital commit. First FizzBuzz ever, yea!

---
+++ FizzBuzz/FizzBuzzPrinter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using ApprovalTests.Core;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzzPrinter
+    {
+        public FizzBuzzPrinter()
+        {
+        }
+
+        internal string PrintLine(int lineNumber)
+        {
+            StringBuilder stringToReturn = new StringBuilder();
+            if (lineNumber % 3 == 0) { stringToReturn.Append("Fizz"); }
+            if (lineNumber % 5 == 0) { stringToReturn.Append("Buzz"); }
+            if (stringToReturn.Length == 0) { return lineNumber.ToString(); }
+            return stringToReturn.ToString();
+            
+        }
+
+        internal string PrintLines(int from, int to)
+        {
+            StringBuilder returnString = new StringBuilder();
+            for (int currentLine = from; currentLine<= to; currentLine++)
+            {
+                returnString.AppendLine(PrintLine(currentLine));
+            }
+            return returnString.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IfLineNumberIsOnePrintOne()
+        {
+            FizzBuzzPrinter fb = new FizzBuzzPrinter();
+            Assert.AreEqual(1.ToString(), fb.PrintLine(1));
+        }
+
+        [TestMethod]
+        public void IfLineNumberIsDivisibleByThreePrintFizz()
+        {
+            FizzBuzzPrinter fb = new FizzBuzzPrinter();
+            Assert.AreEqual("Fizz", fb.PrintLine(3));
+        }
+
+        [TestMethod]
+        public void IfLineNumberIsDivisibleByFivePrintBuzz()
+        {
+            FizzBuzzPrinter fb = new FizzBuzzPrinter();
+            Assert.AreEqual("Buzz", fb.PrintLine(5));
+        }
+
+        [TestMethod]
+        public void IfLineNumberIsDivisibleByThreeAndFivePrintFizzBuzz()
+        {
+            FizzBuzzPrinter fb = new FizzBuzzPrinter();
+            Assert.AreEqual("FizzBuzz", fb.PrintLine(15));
+        }
+
+        [TestMethod]
+        public void PrintLinesOneToOneHundred()
+        {
+            FizzBuzzPrinter fb = new FizzBuzzPrinter();            
+            Approvals.Verify(fb.PrintLines(1, 100));
+        }
+
+    }
+}
